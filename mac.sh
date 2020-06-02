a5a358cb03bd246036a3b3fdfb2b44af384a4c4e
Ethan Ransom <derethanhausen@gmail.com>
2020-06-02 17:40:18 -0600

    split lists onto multiple lines, add spotify

--- mac.sh
+++ mac.sh
@@ -1,1 +1,27 @@
-brew install asdf bat neovim tree tig htop git tmux jq fzf z
+formulae=(
+  asdf
+  bat
+  neovim
+  tree
+  tig
+  htop
+  git
+  tmux
+  jq
+  fzf
+  z
+)
+
+casks=(
+  postico
+  firefox
+  visual-studio-code
+  intel-power-gadget
+  spotify
+  # google-chrome
+  # zoom
+)
+
+brew install "${formulae[@]}"
+
+brew cask install "${casks[@]}"
